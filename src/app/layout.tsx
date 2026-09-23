0563979fdf6925b7411fe4c56be7bc0379a875c5
Pedro <henriqueorlandi87@gmail.com>
2026-09-23 19:51:59 -0300

    Atualiza o titulo para Dc Imports

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next';
+import { ThemeProvider } from 'next-themes';
+import { Onest } from 'next/font/google';
+import './globals.css';
+import { ToasterProvider } from './providers/toaster';
+
+const onest = Onest({
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Dc Imports',
+    template: '%s | Dc Imports',
+  },
+  description:
+'Dc Imports'};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`bg-gray-50 dark:bg-dark-secondary min-h-screen flex flex-col ${onest.className}`}
+      >
+        <ThemeProvider disableTransitionOnChange>
+          {/* ToasterProvider must render before the children components */}
+          {/* https://github.com/emilkowalski/sonner/issues/168#issuecomment-1773734618 */}
+          <ToasterProvider />
+
+          <div className="isolate flex flex-col flex-1">{children}</div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
