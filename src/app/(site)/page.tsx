cfd20a3f223a94440b29fde196b3b0f7c45ee598
Pedro <henriqueorlandi87@gmail.com>
2026-09-26 14:19:48 -0300

    Usa caminho correto public/images para produto 14

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -155,7 +155,7 @@ const productsData: Product[] = [
     concentration: "60mg MD",
     description: "Ampola única de 60mg.",
     manufacturer: "TNL",
-    image:"/images/produto14j.png",
+    image:"public/images/produto14j.png",
     category: "TIRZEPATIDA",
     priceBRL: 680,
   },
