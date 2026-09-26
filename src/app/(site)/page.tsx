3c5f1bcda9a5e5f211a992b01511926c8bfc409f
Pedro <henriqueorlandi87@gmail.com>
2026-09-26 13:46:45 -0300

    Adiciona imagem local

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -155,7 +155,7 @@ const productsData: Product[] = [
     concentration: "60mg MD",
     description: "Ampola única de 60mg.",
     manufacturer: "TNL",
-    image: "https://imgur.com/a/NiOcquf",
+    image:"produto14.png",
     category: "TIRZEPATIDA",
     priceBRL: 680,
   },
